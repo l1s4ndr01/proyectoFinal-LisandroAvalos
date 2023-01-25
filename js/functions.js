a7d6c3ebc71fb149bdeb879bd3357f8f1a86325c
Lisandro Avalosm <lisandronavalos@gmail.com>
2023-01-25 00:10:00 -0300

    maquetado css y js - agrega funcion para desplegar/contraer

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+
+// Agrega al subtitulo contacto la funcion para desplegar el panel con los datos
+
+// document.getElementById('contacto').addEventListener('click', function () {
+//     if(document.getElementById('pnl-contacto').style.display == 'none'){
+//         document.getElementById('pnl-contacto').style.display = 'contents';
+//     }
+//     else{
+//         document.getElementById('pnl-contacto').style.display = 'none';
+//     }
+    
+// });
+
+// funcion para agregar la funcion para desplegar el panel con los datos
+
+// genera una colección con todos los elementos de la clase 'subtitulo'
+const collection = document.getElementsByClassName('subtitulo');
+
+for (let i=0; i < collection.length; i++) {
+
+    let nombreElemento = collection[i].id;
+
+    // a cada elemento le agrega la función que desplega/contrae el panel con la información
+    
+    document.getElementById(nombreElemento).addEventListener('click', function () {
+
+        let nombrePanel = "pnl-"+nombreElemento;
+        console.log("nombre de panel: "+nombrePanel);
+        if(document.getElementById(nombrePanel).style.display == 'none'){
+            document.getElementById(nombrePanel).style.display = 'contents';
+        }
+        else{
+            document.getElementById(nombrePanel).style.display = 'none';
+        }
+        
+    });
+}
+
+
+
